e2061c42d098f54ecd2b1cb7429762d08b5465f6
fadimata tillman <fadi.tillman@gmail.com>
2020-04-21 23:44:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < Sinatra::Base
 
+
+
+
   configure do
   	set :views, "app/views"
   	set :public_dir, "public"
   end
 
-  get "/" do
+  get " /" do
   	erb :index
   end
 end
